4dcc9f39757cfd74d0ab1ca24e635a0fc1dee7e6
Сергей <privet15.89@mail.ru>
2023-09-30 15:33:57 +0500

    simplify imports

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,8 @@
 import {combineReducers} from "redux";
-import {tasksReducer} from "../state/tasks-reducer";
-import {todolistsReducer} from "../state/todolists-reducer";
-import {appReducer} from "../state/app-reducer";
-import {authReducer} from "../state/auth-reducer";
+import {tasksReducer} from "state/tasks-reducer";
+import {todolistsReducer} from "state/todolists-reducer";
+import {appReducer} from "state/app-reducer";
+import {authReducer} from "state/auth-reducer";
 
 export const rootReducer = combineReducers({
     tasks: tasksReducer,
